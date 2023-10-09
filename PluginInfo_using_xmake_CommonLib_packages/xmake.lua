2287587187062923c6ca4d765ffdd022c5d853d4
Mrowr Purr <mrowr.purr@gmail.com>
2023-10-09 15:27:55 -0700

    All versions working

--- PluginInfo_using_xmake_CommonLib_packages/xmake.lua
+++ PluginInfo_using_xmake_CommonLib_packages/xmake.lua
@@ -4,9 +4,7 @@ add_rules("mode.debug", "mode.releasedbg")
 
 add_repositories("SkyrimScripting https://github.com/SkyrimScripting/Packages.git")
 
-add_requires("skyrim-commonlib-ae", "skyrim-commonlib-se", "skyrim-commonlib-vr")
-
--- TODO: don't forget skyrim-commonlib and skyrim-commonlib-ng
+add_requires("skyrim-commonlib-ae", "skyrim-commonlib-se", "skyrim-commonlib-vr", "skyrim-commonlib-ng", "skyrim-commonlib")
 
 target("AE")
     add_files("plugin.cpp")
@@ -40,4 +38,26 @@ target("VR")
         author = "Mrowr Purr",
         email = "mrowr.purr@gmail.com",
         mods_folder = os.getenv("SKYRIM_MODS_VR")
+    })
+
+target("NG")
+    add_files("plugin.cpp")
+    add_packages("skyrim-commonlib-ng")
+    add_rules("@skyrim-commonlib-ng/plugin", {
+        name = "Simple Plugin Custom NG",
+        version = "420.1.69",
+        author = "Mrowr Purr",
+        email = "mrowr.purr@gmail.com",
+        mods_folder = os.getenv("SKYRIM_MODS_AE")
+    })
+
+target("Default CommonLib")
+    add_files("plugin.cpp")
+    add_packages("skyrim-commonlib")
+    add_rules("@skyrim-commonlib/plugin", {
+        name = "Simple Plugin Custom Default CommonLib",
+        version = "420.1.69",
+        author = "Mrowr Purr",
+        email = "mrowr.purr@gmail.com",
+        mods_folder = os.getenv("SKYRIM_MODS_AE")
     })
